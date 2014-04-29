e39239dee41646a6227c51640eac57bd4973920d
Dimitri Roche <dimroc@gmail.com>
2014-04-28 21:11:30 -0400

    Add list of manhattan neighborhoods to main ctrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,35 @@
 
 angular.module('hoodFrequencyApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.hoods = [
+      'east-village',
+      'clinton',
+      'gramercy',
+      'hamilton-heights',
+      'chinatown',
+      'stuyvesant-town-cooper-village',
+      'battery-park-city-lower-manhattan',
+      'yorkville',
+      'midtown-midtown-south',
+      'washington-heights-north',
+      'turtle-bay-east-midtown',
+      'lenox-hill-roosevelt-island',
+      'murray-hill-kips-bay',
+      'upper-east-side-carnegie-hill',
+      'lower-east-side',
+      'central-harlem-north-polo-grounds',
+      'east-harlem-south',
+      'upper-west-side',
+      'lincoln-square',
+      'washington-heights-south',
+      'central-harlem-south',
+      'soho-tribeca-civic-center-little-italy',
+      'west-village',
+      'hudson-yards-chelsea-flatiron-union-square',
+      'manhattanville',
+      'morningside-heights',
+      'east-harlem-north',
+      'marble-hill-inwood',
+      'park-cemetery-etc-manhattan'
+    ]
   });
